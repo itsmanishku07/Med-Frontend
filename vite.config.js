82c11948fa53fc27657fd5870e1260be10c35b50
itsmanishku07 <manish0704ku@gmail.com>
2026-04-04 16:45:28 +0530

    asd

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    host: true
+    host: true,
+    allowedHosts: ['med-frontend-gmeq.onrender.com']
   },
   build: {
     outDir: 'dist',
